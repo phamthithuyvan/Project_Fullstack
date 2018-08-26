89dc571dbba7626cb7b72ca4591a0253a4588c67
LAPTOP-2O43AB1F\pc <phamthuyvan020498@gmail.com>
2018-08-26 13:18:42 +0700

    test1

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,50 @@
 'use strict';
 // Gọi thư viện sử dụng vào
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var babel = require('gulp-babel');
+var plugins = require('gulp-load-plugins')({ lazy: true });
 var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-
-// Copy IMG
-gulp.task('copy', function () {
-  return gulp.src([
-      './src/img/**.*',
-    ])
-    .pipe(gulp.dest('./dist/img'));
-});
-
-// Nối file CSS
-gulp.task('noifile-css', function () {
-  return gulp.src([
-      'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    ])
-    .pipe(concat('thuvien.css'))
-    .pipe(gulp.dest('./dist/css'));
-});
-
-// Nối file JS
-gulp.task('noifile-js', function () {
-  return gulp.src([
-      'bower_components/jquery/dist/jquery.slim.min.js',
-      'bower_components/popper.js/dist/umd/popper.min.js',
-      'bower_components/bootstrap/dist/js/bootstrap.min.js',
-    ])
-    .pipe(concat('thuvien.js'))
-    .pipe(gulp.dest('./dist/js'));
-});
-
-// Task này sẽ tìm tất cả file .sass trong thư mục src/styles sẽ build ra file .css ở thư mục dist/css
-gulp.task('taocss', function () {
-  return gulp.src('./src/styles/**/*.sass')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(browserSync.stream())
-});
-
-// Task này sẽ tìm tất cả file .js trong thư mục src/scripts sẽ build ra file .js ở thư mục dist/js
-gulp.task('taojs', function () {
-  return gulp.src('./src/scripts/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('./dist/js'));
-});
-
-// Task này sẽ tìm tất cả file .pug trong thư mục src/templates sẽ build ra file .html ở thư mục dist
-gulp.task('taohtml', function buildHTML() {
-  return gulp.src([
-      './src/template/**/*.pug',
-      '!./src/template/{**/\_*,**/\_*/**}.pug'
-    ])
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dist'));
-});
-
-// Task này có nhiệm vụ theo dõi mọi thay đổi trên hệ thống
-gulp.task('theodoi', function () {
-  gulp.watch('./src/styles/**/*.sass', ['taocss']);
-  gulp.watch('./src/template/**/*.pug', ['taohtml']);
-  gulp.watch('./src/scripts/**/*.js', ['taojs']);
-  gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
-});
-
-// Static server
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
+var runSequence = require('run-sequence');
+var taskPath = './tasks/';
+var taskList = require('fs').readdirSync(taskPath);
+// Load tất cả các task
+taskList.forEach(function (taskFile) {
+    require(taskPath + taskFile)(gulp, plugins, browserSync);
 });
 
 // Lệnh mặc định của Gulp
-gulp.task('default', function () {
-  gulp.start([
-    'copy',
-    'noifile-css',
-    'noifile-js',
-    'taocss',
-    'taojs',
-    'taohtml',
-    'theodoi',
-    'browser-sync'
-  ]);
+gulp.task('default', function (callback) {
+	runSequence(
+		'clean',
+		'copy-img',
+		'copy-fonts',
+		'copy-favicon',
+		'concat-css',
+		'concat-js',
+		'tao-sass',
+		'tao-js',
+		'tao-html',
+		'watch',
+		'browser-sync',
+		callback
+  );
+});
+gulp.task('build', function (callback) {
+	runSequence(
+		'clean',
+		'copy-img',
+		'copy-fonts',
+		'copy-favicon',
+		'concat-css',
+		'concat-js',
+		'tao-sass',
+		'tao-js',
+		'tao-html',
+		// Xử lý Production
+		'html-min',
+		'css-min',
+		'js-min',
+		'autoprefixer',
+		'browser-sync',
+		callback
+  );
 });
--- readme.md
+++ readme.md
@@ -1,11 +1,45 @@
-# Lấy dự án về máy 
-> git clone https://github.com/fullstackvietnam/fs-01-web-basic.git
-# Chạy dự án
-- Update Node Modules
+```
+    $$$$$$$\   $$$$$$\   $$$$$$\  $$\   $$\  $$$$$$\  $$\   $$\ $$\     $$\ $$$$$$$$\ $$\   $$\
+    $$  __$$\ $$  __$$\ $$  __$$\ $$$\  $$ |$$  __$$\ $$ |  $$ |\$$\   $$  |$$  _____|$$$\  $$ |
+    $$ |  $$ |$$ /  $$ |$$ /  $$ |$$$$\ $$ |$$ /  \__|$$ |  $$ | \$$\ $$  / $$ |      $$$$\ $$ |
+    $$$$$$$\ |$$$$$$$$ |$$ |  $$ |$$ $$\$$ |$$ |$$$$\ $$ |  $$ |  \$$$$  /  $$$$$\    $$ $$\$$ |
+    $$  __$$\ $$  __$$ |$$ |  $$ |$$ \$$$$ |$$ |\_$$ |$$ |  $$ |   \$$  /   $$  __|   $$ \$$$$ |
+    $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |\$$$ |$$ |  $$ |$$ |  $$ |    $$ |    $$ |      $$ |\$$$ |
+    $$$$$$$  |$$ |  $$ | $$$$$$  |$$ | \$$ |\$$$$$$  |\$$$$$$  |    $$ |    $$$$$$$$\ $$ | \$$ |
+    \_______/ \__|  \__| \______/ \__|  \__| \______/  \______/     \__|    \________|\__|  \__|
+
+    Coding by: Bao Nguyen
+    Tel: 0.96.96.89.89.3
+    Email: baonguyenyam@gmail.com
+    URL: fb.com/pham.nguyen.bao.nguyen,
+    baonguyenyam.github.io/cv
+    FullStack Vietnam: https://fullstackvietnam.com/
+```
+# Cài đặt
+
 > npm i 
 
-- Update Bower Componets
+và 
+
+> npm audit fix
+
+Cài đặt Components 
+
 > bower update 
 
-Sau đó để chạy web đánh lệnh 
-> gulp
+# Chạy dự án 
+
+> npm start
+
+# Chạy Production 
+
+> gulp build --production
+
+# Cấu hình
+
+- Xem file `config.json`
+- Xem các files trong thư mục `tasks`
+
+# Tạo Favicon
+
+https://www.favicon-generator.org/
